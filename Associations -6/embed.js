4b15b7cbd5e7bd200a155f93cea48671235afe47
mourice-oduor <otienomaurice364@gmail.com>
2021-01-07 22:11:45 -0500

    Embedding Data

---
+++ Associations -6/embed.js
@@ -0,0 +1,66 @@
+var mongoose = require("mongoose");
+const { getMaxListeners } = require("process");
+mongoose.connect("mongodb://localhost/blog_demo", { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Post: title, content
+var postSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+var Post = mongoose.model("Post", postSchema);
+
+
+// user: email, name
+var userSchema = new mongoose.Schema({
+    email: String,
+    name: String,
+    posts: [postSchema]
+});
+var User = mongoose.model("User", userSchema);
+
+// var newUser = new User ({
+//     email: "steveotieno@gmail.com",
+//     name: "Steven Otieno"
+// });
+
+// newUser.posts.push({
+//     title: "How to master Nodejs",
+//     content: "start by learning basic JavaScript first!!"
+// });
+
+// newUser.save(function(err, user){
+//     if(err){
+//         console.log(err);
+//     } else{
+//         console.log(user);
+//     }
+// });
+
+// var newPost = new Post ({
+//     title: "Node Deployment",
+//     content: "Through Netlify servers"
+// });
+// newPost.save(function(err, post){
+//     if(err){
+//         console.log(err);
+//     } else{
+//         console.log(post);
+//     }
+// });
+
+User.findOne({name: "Steven Otieno"}, function(err, user){
+    if(err){
+        // console.log(err);
+    } else{
+        user.posts.push({
+            title: "Understanding Node.js",
+            content: "Begin by mastering Express-js first!"
+        });
+    } user.save(function(err, user){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(user);
+        }
+    });
+});
