4f83e51ec3caabf2fed247c8727ddf9a740bca5f
mourice-oduor <otienomaurice364@gmail.com>
2021-01-10 12:41:56 -0500

    Update Site

--- Associations -6/references.js
+++ Associations -6/references.js
@@ -14,7 +14,35 @@ var Post = mongoose.model("Post", postSchema);
 var userSchema = new mongoose.Schema({
     email: String,
     name: String,
-    posts: [postSchema]
+    posts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post"
+    }
+    ]
 });
 var User = mongoose.model("User", userSchema);
 
+// User.create({
+//     email: "morrisotieno03@gmail.com",
+//     name: "Morris Otieno"
+// });
+
+Post.create({
+    title: "Understanding Express.js version-3",
+    content: "Do more projects on EJS Concept and repeat until it sinks onto your brain!"
+}, function (err, post) {
+    User.findOne({ email: "morrisotieno03@gmail.com" }, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            foundUser.posts.push(post);
+            foundUser.save(function (err, data) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(data);
+                }
+            });
+        }
+    });
+});
