887278585db6c77f251c8b2cf9e9244c8912bb32
mourice-oduor <otienomaurice364@gmail.com>
2021-01-10 22:03:22 -0500

    Update file contents

---
+++ Associations -6/Module.Exports/models/user.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+
+// user: email, name
+var userSchema = new mongoose.Schema({
+    email: String,
+    name: String,
+    posts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post"
+    }
+    ]
+});
+module.exports = mongoose.model("User", userSchema);
