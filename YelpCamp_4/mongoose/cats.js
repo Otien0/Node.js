8ddf1ba1015e8e40693dc44f812496ff79ad882b
mourice-oduor <otienomaurice364@gmail.com>
2022-03-17 20:04:19 +0300

    Update Scripts

--- YelpCamp_4/mongoose/cats.js
+++ YelpCamp_4/mongoose/cats.js
@@ -13,13 +13,13 @@ var Cat = mongoose.model("Cat", catSchema)
 
 // adding a new cat to the DB
 // when running this, make sure the =>> Cat.create is commented first
-var george = new Cat({
+var myCat = new Cat({
     name: "George",
     age: 15,
     temparament: "Good"
 })
 
-george.save(function (err, cat) {
+myCat.save(function (err, cat) {
     if (err) {
         console.log("SOMETHING WENT WRONG!")
     } else {
@@ -28,17 +28,17 @@ george.save(function (err, cat) {
     }
 });
 
-Cat.create({
-    name: "Snow White",
-    age: 12,
-    temparament: "Bland"
-}, function (err, cat) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log(cat)
-    }
-})
+// Cat.create({
+//     name: "Snow White",
+//     age: 12,
+//     temparament: "Bland"
+// }, function (err, cat) {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log(cat)
+//     }
+// })
 
 //retrieve all cats from the DB and console.log each one
 Cat.find({}, function (err, cats) {
