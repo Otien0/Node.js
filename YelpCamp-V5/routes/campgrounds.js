2e3846629f9a2b433591ec19e514fde054c1b326
mourice-oduor <otienomaurice364@gmail.com>
2022-03-22 18:31:49 +0300

    Update Campground.js file

--- YelpCamp-V5/routes/campgrounds.js
+++ YelpCamp-V5/routes/campgrounds.js
@@ -1,5 +1,6 @@
-const express = require("express"),
-      router  = express.Router();
+const express    = require("express"),
+      router     = express.Router(),
+      Campground = require("../models/campground");
 
 
 
