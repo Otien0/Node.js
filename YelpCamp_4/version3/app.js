bfddcc52f5f3581b3a5011963b94933c59f3edee
mourice-oduor <otienomaurice364@gmail.com>
2022-03-22 11:35:50 +0300

    Update YelpCamp Contents

--- YelpCamp_4/version3/app.js
+++ YelpCamp_4/version3/app.js
@@ -1,20 +1,22 @@
-var express    = require("express"),
+const seedDB = require("./seeds"),
+    express    = require("express"),
     app        = express(),
     bodyParser = require("body-parser"),
     mongoose   = require("mongoose"),
-    Campground = require("./models/campground");
+    Campground = require("./models/campground"),
+    Comment    = require("./models/comment");
 
 mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-
+seedDB();
 
 
 // Campground.create(
 //     {
-//         name: "Camp Malta", 
-//         image: "https://i.imgur.com/uEepnRVb.jpg",
+//         name: "Rapids Camp, Sagana", 
+//         image: "https://i.imgur.com/8G5IehWb.jpg",
 //         description: "Camp friendly to your wallet. You will get a tent, cooking utensils, refrigeration, cutlery, gas burners, jikos, a barbeque burner, and meeting facilities like chairs and whiteboards. Facilities which are provided at an additional fee include charcoal, firewood for the camp fire, sleeping bags, cooking gas and a cook or a chef."
 //     }, 
 //     function(err, campground){
@@ -32,7 +34,7 @@ app.use(express.static("public"));
 //     { name: "Camp Ya Kanzi", image: "https://i.imgur.com/a785ZXCb.jpg" },
 //     { name: "Rapids Camp, Sagana", image: "https://i.imgur.com/8G5IehWb.jpg" },
 //     { name: "Malewa Bush Ventures", image: "https://i.imgur.com/1ZqCkDEb.jpg" },
-//     { name: "Camp Carnelley’s", image: "https://i.imgur.com/eTOogftb.jpg" },
+//     { name: "Camp Carnelleyï¿½s", image: "https://i.imgur.com/eTOogftb.jpg" },
 //     { name: "El Karama", image: "https://i.imgur.com/4RSDCkkb.jpg" },
 //     { name: "Kiboko Camp", image: "https://i.imgur.com/J9Gp9cKb.jpg" },
 //     { name: "Mamba Village", image: "https://i.imgur.com/eUonOB7s.jpg" },
@@ -53,7 +55,7 @@ app.get("/campgrounds", function(req, res){
         if(err){
             console.log(err);
         } else{
-            res.render("index", {campgrounds: allCampgrounds})
+            res.render("campgrounds/index", {campgrounds: allCampgrounds})
         }
     });
     
@@ -80,20 +82,50 @@ app.post("/campgrounds", function(req, res){
 
 //NEW ROUTE-- show form to create new campground
 app.get("/campgrounds/new", function(req, res){
-    res.render("new.ejs")
+    res.render("campgrounds/new")
 })
 
 //SHOW ROUTE-- shows more info about one campground
 app.get("/campgrounds/:id", function(req, res){
     //find the campground with provided ID
-    Campground.findById(req.params.id, function(err, foundCampground){
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
         if(err){
-            console.log(err)
+            console.log(err);
         } else {
-            //render show template with that campgound
-            res.render("show", {campground: foundCampground})
+            //render show template with that campground
+            res.render("campgrounds/show", {campground: foundCampground});
         }
-    })  
+    });  
+});
+
+//Comments ROUTE
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            })
+        }
+    })
 })
 
 app.listen(3000, function(req, res){
