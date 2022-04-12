fcc78076a164ce1dddf6e4b0786ce1f401628431
mourice-oduor <otienomaurice364@gmail.com>
2022-04-12 04:53:22 +0300

    Update User Schema

--- YelpCamp-V7/models/user.js
+++ YelpCamp-V7/models/user.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
+const mongoose = require('mongoose'),
+      Schema = mongoose.Schema,
+      passportLocalMongoose = require('passport-local-mongoose');
 
-const User = new Schema({
-    username: String,
-    password: String
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
 });
 
-User.plugin(passportLocalMongoose);
+UserSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('User', User);
+module.exports = mongoose.model('User', UserSchema);
