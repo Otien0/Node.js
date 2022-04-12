17de293ed8094f5fd664c11082bf785300d652fd
mourice-oduor <otienomaurice364@gmail.com>
2022-04-12 18:07:17 +0300

    update seeds data

--- YelpCamp-V7/seeds/index.js
+++ YelpCamp-V7/seeds/index.js
@@ -27,9 +27,18 @@ const seedDB = async () => {
             author: '623be4ff42b0af2f006c3118',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
-            price
+            description: 'Campi ya Kanzi was born in 1975 when Luca first set foot in the Chyulu. It was a childhood dream for 20 years, and become a reality in 1995 when Luca & Antonella moved to the Chyulu permanently. It took a few years to build, during which time we lived in a small tent for more than 24 months… The toilet was a pit in the bush and the shower a bucket on a tree, with stars as the canopy… Luca misses it, Antonella still damages his credit card in retaliation!',
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/self-hosted/image/upload/v1649773314/YelpCamp/vaqjuohnqebigakdgki3.jpg',
+                    filename: 'YelpCamp/vaqjuohnqebigakdgki3'
+                },
+                {
+                    url: 'https://res.cloudinary.com/self-hosted/image/upload/v1649773315/YelpCamp/stpgwysdsuxeipf7j8qu.jpg',
+                    filename: 'YelpCamp/stpgwysdsuxeipf7j8qu'
+                }
+            ]
         })
         await camp.save();
     }
