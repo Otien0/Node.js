99f893dbb03b89357776d9ba41387aa61d6ac413
mourice-oduor <otienomaurice364@gmail.com>
2022-04-12 18:04:54 +0300

    update index file

--- YelpCamp-V7/cloudinary/index.js
+++ YelpCamp-V7/cloudinary/index.js
@@ -1,10 +1,15 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_KEY,
+//     api_secret: process.env.CLOUDINARY_SECRET
+// });
+cloudinary.config({ 
+    cloud_name: 'self-hosted', 
+    api_key: '285568845756964', 
+    api_secret: 'iCoud1L7mbrSI1T3XcSjkT01YUs' 
 });
 
 const storage = new CloudinaryStorage({
