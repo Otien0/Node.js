4d72b0ae4747c1bba4de5f9ca06e61442c9e195c
mourice-oduor <otienomaurice364@gmail.com>
2022-04-12 18:06:14 +0300

    Update Script

--- YelpCamp-V7/public/javascripts/validateForms.js
+++ YelpCamp-V7/public/javascripts/validateForms.js
@@ -1,6 +1,8 @@
 (function () {
     'use strict'
 
+    bsCustomFileInput.init()  //Display file names for multiple input
+
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.validated-form')
 
