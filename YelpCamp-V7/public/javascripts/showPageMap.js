f750e5e776dcf832fca2915268fb324ff504264c
mourice-oduor <otienomaurice364@gmail.com>
2022-04-13 22:10:19 +0300

    added navigation controls

--- YelpCamp-V7/public/javascripts/showPageMap.js
+++ YelpCamp-V7/public/javascripts/showPageMap.js
@@ -6,6 +6,8 @@ const map = new mapboxgl.Map({
     zoom: 10 // starting zoom
 });
 
+map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
     .setPopup(
