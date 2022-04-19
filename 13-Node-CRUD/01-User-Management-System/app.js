7d58bb05a07a2cd51bcc97428ee570749c8eb5ad
mourice-oduor <otienomaurice364@gmail.com>
2022-04-19 22:13:45 +0300

    update app.js

--- 13-Node-CRUD/01-User-Management-System/app.js
+++ 13-Node-CRUD/01-User-Management-System/app.js
@@ -1,21 +1,31 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express       = require('express'),
+      app           = express(),
+      mongoose      = require("mongoose"),
       dotenv        = require('dotenv'),
       morgan        = require('morgan'),
       bodyparser    = require("body-parser"),
       path          = require('path');
 
-const connectDB = require('./server/database/connection');
-
-const app = express();
-
-dotenv.config( { path : 'config.env'} )
-const PORT = process.env.PORT || 3000
+const dbUrl         = process.env.DB_URL || 'mongodb://localhost/user-management';
+// const dbUrl      = "mongodb://localhost/user-management";
 
 // log requests
 app.use(morgan('tiny'));
 
-// mongodb connection
-connectDB();
+// "mongodb://localhost/yelp_camp"
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 // parse request to body-parser
 app.use(bodyparser.urlencoded({ extended : true}))
@@ -32,4 +42,12 @@ app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 // load routers
 app.use('/', require('./server/routes/router'))
 
-app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`User-Management server running on port ${PORT}`);
+});
+
+// app.listen(3000, () => {
+//     console.log("YelpCamp server running on port 3000")
+// })
+// app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
