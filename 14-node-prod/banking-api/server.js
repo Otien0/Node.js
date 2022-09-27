973f7a812719fcb641971b92bc57dadea2083472
mourice-oduor <otienomaurice364@gmail.com>
2022-09-27 16:39:42 +0300

    Update server file

---
+++ 14-node-prod/banking-api/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+
+const accounts = require('./routes/accounts');
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+
+app.use('/api/v1/accounts', accounts);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
