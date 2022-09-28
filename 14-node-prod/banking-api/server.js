3b5f62c4e4b5c5953a95bc9929c949f895c5829b
mourice-oduor <otienomaurice364@gmail.com>
2022-09-28 12:28:54 +0300

    Updae app

--- 14-node-prod/banking-api/server.js
+++ 14-node-prod/banking-api/server.js
@@ -15,10 +15,9 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-
 app.use('/api/v1/accounts', accounts);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
